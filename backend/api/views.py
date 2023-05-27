857cd1e2f57a154a54e0d2022c557a2a2ccd7671
Екатерина Толмачева <kathy.tolmacheva@gmail.com>
2023-05-27 20:47:04 +0300

    22222222

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         IsAuthenticated)
-from rest_framework import filters
-from rest_framework.pagination import (LimitOffsetPagination)
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
