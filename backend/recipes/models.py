5e7d2d81feeab2c18571efc6fca198176e1c8748
Екатерина Толмачева <kathy.tolmacheva@gmail.com>
2023-06-03 14:40:54 +0300

    123

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
+from django.shortcuts import get_object_or_404
 from foodgram.settings import MIN_VALUE_MINUTES, MIN_VALUE_AMOUNT
 
 User = get_user_model()
